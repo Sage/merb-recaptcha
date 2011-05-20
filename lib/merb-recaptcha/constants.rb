2d2bcc90a9589cd735723880234b671959a18d1e
Richard Zhou <richardzhouca@gmail.com>
2011-05-20 10:43:46 -0700

    Change to http for API_VERIFY_SERVER since it does not like htpps

--- lib/merb-recaptcha/constants.rb
+++ lib/merb-recaptcha/constants.rb
@@ -2,6 +2,6 @@ module Merb
   module Recaptcha
     API_SERVER = "http://www.google.com/recaptcha/api"
     API_SECURE_SERVER = "https://www.google.com/recaptcha/api"
-    API_VERIFY_SERVER = "https://www.google.com/recaptcha/api"
+    API_VERIFY_SERVER = "http://www.google.com/recaptcha/api"
   end
 end
